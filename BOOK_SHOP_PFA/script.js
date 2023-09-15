b0d0259dcc0db5398a046dfd6dffa92185ff91e7
AhmedFattachi <123905302+AhmedFattachi@users.noreply.github.com>
2023-09-15 03:31:04 +0100

    Add files via upload

--- BOOK_SHOP_PFA/script.js
+++ BOOK_SHOP_PFA/script.js
@@ -5,17 +5,16 @@ toggle.addEventListener('click', function () {
     body.classList.toggle('open');
 })
 
-
 const carousel = document.querySelector('.content');
 const cards = document.querySelectorAll('.carte-populair-books');
 const prevButton = document.querySelector('.left');
 const nextButton = document.querySelector('.right');
-const cardWidth = cards[0].offsetWidth + 10; 
-const cardMargin = 10; 
+const cardWidth = cards[0].offsetWidth + 10;
+const cardMargin = 10;
 let currentIndex = 0;
-
-
 const numCards = cards.length;
+
+// Clonez les cartes et ajoutez-les à la fin du carrousel
 const clonedCards = [...cards].map(card => card.cloneNode(true));
 clonedCards.forEach(clonedCard => {
     const wrapper = document.createElement('div');
@@ -25,12 +24,20 @@ clonedCards.forEach(clonedCard => {
 });
 
 nextButton.addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % (numCards + 1); // 
+    if (currentIndex < numCards - 1) {
+        currentIndex++;
+    } else {
+        currentIndex = 0;
+    }
     updateCarousel();
 });
 
 prevButton.addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + numCards + 1) % (numCards + 1);
+    if (currentIndex > 0) {
+        currentIndex--;
+    } else {
+        currentIndex = numCards - 1;
+    }
     updateCarousel();
 });
 
@@ -39,11 +46,23 @@ function updateCarousel() {
     carousel.style.transition = 'transform 0.5s ease-in-out';
     carousel.style.transform = `translateX(${translateX}px)`;
 
-    if (currentIndex === numCards) {
-        setTimeout(() => {
-            carousel.style.transition = 'none';
-            currentIndex = 0;
-            updateCarousel();
-        }, 600);
+    // Gestion de la visibilité des boutons
+    if (currentIndex === 0) {
+        prevButton.style.display = 'none';
+        nextButton.style.display = 'block';
+    } else if (currentIndex === numCards - 3) {
+        prevButton.style.display = 'block';
+        nextButton.style.display = 'none';
+    } else {
+        prevButton.style.display = 'block';
+        nextButton.style.display = 'block';
     }
 }
+
+// Gestion initiale de la visibilité des boutons
+prevButton.style.display = 'none';
+if (numCards <= 1) {
+    nextButton.style.display = 'none';
+}
+
+
