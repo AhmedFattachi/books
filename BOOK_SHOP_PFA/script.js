31695ad70c228919a5ca4558bdc511aeab5ed1a4
AhmedFattachi <123905302+AhmedFattachi@users.noreply.github.com>
2023-09-16 04:06:28 +0100

    Add files via upload

--- BOOK_SHOP_PFA/script.js
+++ BOOK_SHOP_PFA/script.js
@@ -62,4 +62,59 @@ if (numCards <= 1) {
     nextButton.style.display = 'none';
 }
 
+const carousel2 = document.querySelector('.content-bs'); // Remplacez '.content2' par le sélecteur approprié pour le deuxième carousel
+const cards2 = document.querySelectorAll('.carte-bestsellers-books'); // Remplacez '.carte-populair-books2' par le sélecteur approprié pour les cartes du deuxième carousel
+const prevButton2 = document.querySelector('.left-bs'); // Remplacez '.left2' par le sélecteur approprié pour le bouton précédent du deuxième carousel
+const nextButton2 = document.querySelector('.right-bs'); // Remplacez '.right2' par le sélecteur approprié pour le bouton suivant du deuxième carousel
+const cardWidth2 = cards2[0].offsetWidth + 10; // Ajustez la largeur de la carte du deuxième carousel si nécessaire
+const cardMargin2 = 10; 
+let currentIndex2 = 0;
+const numCards2 = cards2.length;
 
+const clonedCards2 = [...cards2].map(card => card.cloneNode(true));
+clonedCards2.forEach(clonedCard => {
+    const wrapper2 = document.createElement('div');
+    wrapper2.classList.add('card-wrapper');
+    wrapper2.appendChild(clonedCard);
+    carousel2.appendChild(wrapper2);
+});
+
+nextButton2.addEventListener('click', () => {
+    if (currentIndex2 < numCards2 - 1) {
+        currentIndex2++;
+    } else {
+        currentIndex2 = 0;
+    }
+    updateCarousel2();
+});
+
+prevButton2.addEventListener('click', () => {
+    if (currentIndex2 > 0) {
+        currentIndex2--;
+    } else {
+        currentIndex2 = numCards2 - 1;
+    }
+    updateCarousel2();
+});
+
+function updateCarousel2() {
+    const translateX2 = -currentIndex2 * (cardWidth2 + cardMargin2);
+    carousel2.style.transition = 'transform 0.5s ease-in-out';
+    carousel2.style.transform = `translateX(${translateX2}px)`;
+
+    if (currentIndex2 === 0) {
+        prevButton2.style.display = 'none';
+        nextButton2.style.display = 'block';
+    } else if (currentIndex2 === numCards2 - 3) {
+        prevButton2.style.display = 'block';
+        nextButton2.style.display = 'none';
+    } else {
+        prevButton2.style.display = 'block';
+        nextButton2.style.display = 'block';
+    }
+}
+
+prevButton2.style.display = 'none';
+if (numCards2 <= 1) {
+    nextButton2.style.display = 'none';
+}
